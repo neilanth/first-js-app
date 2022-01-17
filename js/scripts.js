ab74493c383e8d36b934fd7e4e928c14d17414ae
neilanth <neilanthony99@gmail.com>
2022-01-17 10:16:54 -0800

    Added an array with 3 Pokemon.
    Removed old JS code, added new array with Pokemon content needed for building the final application.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
-alert('Hello world');
-let favoriteFood='Pasta';
-document.write (favoriteFood);
+let pokemonList = [
+  {
+    name: "Charmander",
+    height: 2.0,
+    types: [fire, water, ground, rock],
+  },
+
+  { name: "Blastoise", height: 5.03, types: [water, grass, electric] },
+  { name: "Metapod", height: 2.04, types: [bug, fire, flying, rock] },
+];
