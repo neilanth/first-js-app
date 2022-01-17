17249b148d6ee9d6bbc10b8cba2f03023cba43b8
neilanth <neilanthony99@gmail.com>
2022-01-17 11:14:25 -0800

    Added Quotation marks to Pokemon string values.

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,11 @@ let pokemonList = [
   {
     name: "Charmander",
     height: 2.0,
-    types: [fire, water, ground, rock],
+    types: [fire, "water", "ground", "rock"],
   },
 
-  { name: "Blastoise", height: 5.03, types: [water, grass, electric] },
-  { name: "Metapod", height: 2.04, types: [bug, fire, flying, rock] },
+  { name: "Blastoise", height: 5.03, types: ["water", "grass", "electric"] },
+  { name: "Metapod", height: 2.04, types: ["bug", "fire", "flying", "rock"] },
 ];
+
+console.log
